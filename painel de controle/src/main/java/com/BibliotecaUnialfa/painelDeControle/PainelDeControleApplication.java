9ac6ae03eb7c4812a17bf4f6e17700eac5c005f6
UniALFA <unialfa@MACMINI-02.local>
2023-06-19 20:45:54 -0300

    Criado base springBoot de projeto

---
+++ painel de controle/src/main/java/com/BibliotecaUnialfa/painelDeControle/PainelDeControleApplication.java
@@ -0,0 +1,13 @@
+package com.BibliotecaUnialfa.painelDeControle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainelDeControleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainelDeControleApplication.class, args);
+	}
+
+}
