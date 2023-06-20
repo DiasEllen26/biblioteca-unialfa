dff23318497b54e704da5946d14b1442b8d7b880
DiasEllen26 <106031187+MarcoAL2000@users.noreply.github.com>
2023-06-19 22:10:20 -0300

    Iniciando formulário login

--- biblioteca/paginas/login.php
+++ biblioteca/paginas/login.php
@@ -1,1 +1,75 @@
-testes
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Biblioteca UniAlfa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/login.css">
+    <!-- Fonts!-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open%20Sans&display=swap" rel="stylesheet">
+</head>
+  <body>
+  <section class="h-100 gradient-form" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-10">
+        <div class="card rounded-3 text-black">
+          <div class="row g-0">
+            <div class="col-lg-6">
+              <div class="card-body p-md-5 mx-md-4">
+
+                <div class="text-center">
+                  <img src="../imagens/logo.png"
+                    style="width: 185px;" alt="logo">
+                  <h4 class="mt-1 mb-5 pb-1">Seja bem vindo a Biblioteca!</h4>
+                </div>
+
+                <form>
+                  <p>Por favor, entre em sua conta:</p>
+
+                  <div class="form-outline mb-4">
+                    <input type="email" id="" class="form-control" placeholder="Digite seu RA..." />
+                    <label class="form-label" for="form2Example11">RA</label>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="password" id="" class="form-control" placeholder="Digite sua senha..." />
+                    <label class="form-label" for="form2Example22">Senha</label>
+                  </div>
+
+                  <div class="text-center pt-1 mb-5 pb-1">
+                    <button class="btn btn-primary btn-block fa-lg mb-3" type="button">Entrar</button>
+                  </div>
+
+                  <div class="d-flex align-items-center justify-content-center pb-4">
+                    <p class="mb-0 me-2">Não possui conta?</p>
+                    <button type="button" class="btn btn-outline-info">Criar agora</button>
+                  </div>
+
+                </form>
+
+              </div>
+            </div>
+            
+            <div class="col-lg-6 d-flex align-items-center text-light gradient-custom-2">
+              <div class="banner px-3 py-4 p-md-5 mx-md-4">
+                <p class="h2">BIBLIOTECA ALFA</p>
+                <p class="h4">na palma da sua</p>
+                <p class="h4">mão.</p>
+                <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus amet aut non molestias, qui laudantium ratione quia. Quo placeat et dolorem voluptas. Nemo quam vero error, laborum consequatur commodi doloremque?</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
