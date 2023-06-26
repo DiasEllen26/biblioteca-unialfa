3b99d04af25a4ca97c826def4148fa3b62470c78
VitorInacio <vitorinacio16@gmail.com>
2023-06-25 22:37:07 -0300

    listar/alunos + algumas mudanças no index

--- biblioteca/config.php
+++ biblioteca/config.php
@@ -1,11 +1,11 @@
 <?php
     $servidor = "localhost";
     $usuario  = "root";
-    $senha    = "root"; 
+    $senha    = ""; 
     $banco    = "biblioteca-unialfa";
 
-    try {
-        $pdo = new PDO("mysql:host={$servidor};dbname={$banco};port=8889;charset=utf8;",$usuario,$senha);
+    try {                                                    //port=8889 ou 3306
+        $pdo = new PDO("mysql:host={$servidor};dbname={$banco};port=3306;charset=utf8;",$usuario,$senha);
     } catch (Exception $e) {
         echo "<p>Erro ao tentar conectar</p>";
         echo $e->getMessage();
